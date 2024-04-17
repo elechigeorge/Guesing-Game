ff89553cf13262242ebfbc3b47b4e0b50015b37c
George Elechi <20012351-GeorgeElechi@users.noreply.replit.com>
2024-04-17 16:12:18 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string word = "fiddle";
+        bool isCorrect = false;
+        string gueseWord;
+        int count = 0;
+
+        Console.WriteLine("Welcome to .Net Guesing Game.");
+
+
+        while (isCorrect == false && count < 5)
+        {
+            Console.WriteLine("Type your guesing word ");
+            gueseWord = Console.ReadLine();
+
+            if (gueseWord == word)
+            {
+                Console.WriteLine("Congrat! Your guese is right...");
+                isCorrect = true;
+            }
+            else
+            {
+                Console.WriteLine("Oh Snap! Your guese is wrong...");
+                count = count + 1;
+            }
+        }
+
+        Console.WriteLine("Oh.. You have exceeded your guese limit... Try restarting the console");
+
+
+    }
+}
